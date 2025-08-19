50c23fa2fa73226e5bdbc25ae705aca439a284c1
Mateus <mferr7524@gmail.com>
2025-08-19 10:05:45 -0300

    Add files via upload

---
+++ Projeto em JavaScript e REACT/script.js
@@ -0,0 +1,93 @@
+// // alert: print do Python  serce para avisar o usuario sobre algo
+// alert("hello word")
+// alert("Ola turma 19,bora que vamos em frontend")
+
+
+// //prompt; ele e como input do python
+
+// prompt("qual e sua materia favorita, hein")
+
+// //confirm: mostra duas opção o ok e cancela 
+
+// confirm("e front ne ")
+
+
+// //let: variaveis que variam, valores flexiveis 
+
+// //const: contantes sao valores fixos , com o pip(3,14)
+// //Var:essa aqui voces vao ver em codigos antigos e nao e mais conhecido como uma boa pratica
+
+// let materia= prompt("qual e sua materia favorita, alem do Dori")
+
+// alert(materia)
+
+
+
+
+//front end
+//JS:WEB 1.0 (AINDA APRIMORANDO A COMUNICAÇÃO ENTRE COMPUTADORES - PAGINA QUE PARECIA WIKIPEDIA CHATOOO)
+//WEB2.0 COMEÇAMOS A APRIMORAR HTML CSS TIPO O SITE ANTIGO DA UOL GOOGLE E MICROSOFT
+// WEB 3.0 ESTAMOS SAINDO DESSA ERA, MAS O GRANDE AQUI E A INTERAÇÃO COM O USUARIO, PARA ISSO FOI NECESSARIO A CRIAÇÃO DE UMA CRIAÇÃOE UMA LINGUAGEM DE PROGRAMAÇÃO 
+// O FAMOSO, FABULOSO, EXTRAORDINARIO JavaScript
+
+
+//WEB 4.0-IA, IOT,AUTOMAÇÃO,ROBOTIZAÇÃO, SEGURANÇA
+
+
+//TRES MECANISMO PARA FAZER AS PRIMEIOS INTERACÇÃO COM O USUARIOS
+
+
+
+
+// SERVE PARA MOSTRAR O QUE VOCE QUER COMINCAR COM O USUARIO 
+//alert("OLA MUNDO COMO VOCE ESTA ")
+
+//PROMPT E UNA FORMA DE FAZER PERGUTA AO USUARIO  COLETANDO INFORMAÇÃO
+
+//prompt("e ai so de boa ")
+
+//cofirme ele da das opçoes ok ou cancela 
+//confirm("a minha aula e demais ne?")
+
+
+//variavel
+
+//let, const, var
+//let: valores e variam valores fleziveis 
+//const: valores fixos (tipo pi(3,1416))
+//var: 1 forma de declarar variaveis, codigos
+//legados
+
+//let nome= prompt("qual e seu nome")
+//alert(nome)
+
+
+
+
+alert("ola boas vindas ")
+let nome= prompt("qual e seu nome?")
+alert(nome)
+
+alert("qual e sua dsciplina favorita")
+let digita=prompt("informe sua disciplina favorita")
+alert(digita)
+
+alert("qual e data de hoje")
+let data = prompt("qual e data de hoje")
+alert(data)
+
+alert("voce esta preparado para segundo semestre para deselvovimento de sistemas")
+let digite = prompt("sim ou nao")
+alert(digite)
+
+// nedia de 2 notas 
+
+let=nota1=prompt("qual e sua primeira nota")
+
+let=nota2=prompt("qual e sua segunda nota")
+
+let resultado = (nota1+nota2)/2
+
+alert ("a sua media e de ${resultado)")
+
+

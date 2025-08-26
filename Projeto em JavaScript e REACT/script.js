4fe0a626fb94b0e97358514d26831f8c05f68cb6
Mateus <mferr7524@gmail.com>
2025-08-26 11:24:56 -0300

    Update script.js

--- Projeto em JavaScript e REACT/script.js
+++ Projeto em JavaScript e REACT/script.js
@@ -1,94 +1,237 @@
 // // alert: print do Python  serce para avisar o usuario sobre algo
-// alert("hello word")
-// alert("Ola turma 19,bora que vamos em frontend")
+// // alert("hello word")
+// // alert("Ola turma 19,bora que vamos em frontend")
 
 
-// //prompt; ele e como input do python
+// // //prompt; ele e como input do python
 
-// prompt("qual e sua materia favorita, hein")
+// // prompt("qual e sua materia favorita, hein")
 
-// //confirm: mostra duas opção o ok e cancela 
+// // //confirm: mostra duas opção o ok e cancela 
 
-// confirm("e front ne ")
-
-
-// //let: variaveis que variam, valores flexiveis 
-
-// //const: contantes sao valores fixos , com o pip(3,14)
-// //Var:essa aqui voces vao ver em codigos antigos e nao e mais conhecido como uma boa pratica
-
-// let materia= prompt("qual e sua materia favorita, alem do Dori")
-
-// alert(materia)
+// // confirm("e front ne ")
 
 
+// // //let: variaveis que variam, valores flexiveis 
 
+// // //const: contantes sao valores fixos , com o pip(3,14)
+// // //Var:essa aqui voces vao ver em codigos antigos e nao e mais conhecido como uma boa pratica
 
-//front end
-//JS:WEB 1.0 (AINDA APRIMORANDO A COMUNICAÇÃO ENTRE COMPUTADORES - PAGINA QUE PARECIA WIKIPEDIA CHATOOO)
-//WEB2.0 COMEÇAMOS A APRIMORAR HTML CSS TIPO O SITE ANTIGO DA UOL GOOGLE E MICROSOFT
-// WEB 3.0 ESTAMOS SAINDO DESSA ERA, MAS O GRANDE AQUI E A INTERAÇÃO COM O USUARIO, PARA ISSO FOI NECESSARIO A CRIAÇÃO DE UMA CRIAÇÃOE UMA LINGUAGEM DE PROGRAMAÇÃO 
-// O FAMOSO, FABULOSO, EXTRAORDINARIO JavaScript
+// // let materia= prompt("qual e sua materia favorita, alem do Dori")
 
-
-//WEB 4.0-IA, IOT,AUTOMAÇÃO,ROBOTIZAÇÃO, SEGURANÇA
-
-
-//TRES MECANISMO PARA FAZER AS PRIMEIOS INTERACÇÃO COM O USUARIOS
+// // alert(materia)
 
 
 
 
-// SERVE PARA MOSTRAR O QUE VOCE QUER COMINCAR COM O USUARIO 
-//alert("OLA MUNDO COMO VOCE ESTA ")
-
-//PROMPT E UNA FORMA DE FAZER PERGUTA AO USUARIO  COLETANDO INFORMAÇÃO
-
-//prompt("e ai so de boa ")
-
-//cofirme ele da das opçoes ok ou cancela 
-//confirm("a minha aula e demais ne?")
+// //front end
+// //JS:WEB 1.0 (AINDA APRIMORANDO A COMUNICAÇÃO ENTRE COMPUTADORES - PAGINA QUE PARECIA WIKIPEDIA CHATOOO)
+// //WEB2.0 COMEÇAMOS A APRIMORAR HTML CSS TIPO O SITE ANTIGO DA UOL GOOGLE E MICROSOFT
+// // WEB 3.0 ESTAMOS SAINDO DESSA ERA, MAS O GRANDE AQUI E A INTERAÇÃO COM O USUARIO, PARA ISSO FOI NECESSARIO A CRIAÇÃO DE UMA CRIAÇÃOE UMA LINGUAGEM DE PROGRAMAÇÃO 
+// // O FAMOSO, FABULOSO, EXTRAORDINARIO JavaScript
 
 
-//variavel
+// //WEB 4.0-IA, IOT,AUTOMAÇÃO,ROBOTIZAÇÃO, SEGURANÇA
 
-//let, const, var
-//let: valores e variam valores fleziveis 
-//const: valores fixos (tipo pi(3,1416))
-//var: 1 forma de declarar variaveis, codigos
-//legados
 
-//let nome= prompt("qual e seu nome")
-//alert(nome)
+// //TRES MECANISMO PARA FAZER AS PRIMEIOS INTERACÇÃO COM O USUARIOS
 
 
 
 
-alert("ola boas vindas ")
-let nome= prompt("qual e seu nome?")
-alert(nome)
+// // SERVE PARA MOSTRAR O QUE VOCE QUER COMINCAR COM O USUARIO 
+// //alert("OLA MUNDO COMO VOCE ESTA ")
 
-alert("qual e sua dsciplina favorita")
-let digita=prompt("informe sua disciplina favorita")
-alert(digita)
+// //PROMPT E UNA FORMA DE FAZER PERGUTA AO USUARIO  COLETANDO INFORMAÇÃO
 
-alert("qual e data de hoje")
-let data = prompt("qual e data de hoje")
-alert(data)
+// //prompt("e ai so de boa ")
 
-alert("voce esta preparado para segundo semestre para deselvovimento de sistemas")
-let digite = prompt("sim ou nao")
-alert(digite)
+// //cofirme ele da das opçoes ok ou cancela 
+// //confirm("a minha aula e demais ne?")
 
-// nedia de 2 notas 
-let=nota1=Number(prompt)("qual e sua segunda nota")
 
-let=nota2=  Number(prompt)("qual e sua segunda nota")
+// //variavel
 
-let resultado = (nota1+nota2)/2
+// //let, const, var
+// //let: valores e variam valores fleziveis 
+// //const: valores fixos (tipo pi(3,1416))
+// //var: 1 forma de declarar variaveis, codigos
+// //legados
 
-alert ("a sua media e de ${resultado)")
+// //let nome= prompt("qual e seu nome")
+// //alert(nome)
 
 
 
 
+// // alert("ola boas vindas ")
+// // let nome= prompt("qual e seu nome?")
+// // alert(nome)
+
+// // alert("qual e sua dsciplina favorita")
+// // let digita=prompt("informe sua disciplina favorita")
+// // alert(digita)
+
+// // alert("qual e data de hoje")
+// // let data = prompt("qual e data de hoje")
+// // alert(data)
+
+// // alert("voce esta preparado para segundo semestre para deselvovimento de sistemas")
+// // let digite = prompt("sim ou nao")
+// // alert(digite)
+
+// // // nedia de 2 notas 
+// // let=nota1=Number(prompt)("qual e sua segunda nota")
+
+// // let=nota2=  Number(prompt)("qual e sua segunda nota")
+
+// // let resultado = (nota1+nota2)/2
+
+// // alert ("a sua media e de ${resultado)")
+
+
+
+// // let soma=prompt("nota1")
+// // let soma2=prompt("nota2")
+// // let resultado=(nota1+nota2)/2
+// // alert(`a sua media e ${resultado}`)
+
+
+// // let prova1= prompt("prova1")
+// // let prova2=prompt("prova2")
+// // let prova3=prompt("nota3")
+// // let resultado=(prova1+prova2+prova3)/2
+// // alert (`exibir ${resultado}`)
+
+
+
+
+
+// // let  a18=prompt(`maior de idade `)
+// // let a17=prompt(`menor de idade`)
+// // alert(`voce e maior de idade ou menor de idade ${resultado}`)
+
+// //condicionais:  se normalmente eu ainda um senao, e isso pode ser programado
+// // a estrutura aqui js e if e else com chava 
+
+// // if (resultado >18 <18){
+// //    alert('qual e sua idade ')
+// //     prompt('voce e menor de idade sim ou nao ')
+
+// //     }else {
+// //         alert('qual e sua idade ')
+      
+// //        prompt('voce e maior de idade sim ou nao ')       
+// // }
+
+
+// // if(resultado){
+// //     alert('numero e par?')
+// //     prompt('esse numero e par! ')
+// // }else{
+// //     alert('esse numero e impar? ')
+// //     prompt('esse numero e impar! ')
+
+
+// // }
+
+
+// // if(resultado){
+// //     alert('digite dois numeros')
+// //     prompt('ele e maior ')
+
+// // }else{
+// //     alert('digite o numero')
+// //     prompt('eles sao iguais')
+
+
+// // }
+
+// //5
+// // if(senha){
+// //     alert('digite a senha')
+// //     prompt('a senha e igual')
+
+// //  } else{
+// //     alert('a senha esta incorreta ')
+// //     prompt('tente novamente')
+
+// // }
+
+// // alert('temperatura atual em graus de Celsius')
+// // prompt('qual e temperatura?')
+// // if(frio){
+// //     alert('se fpr menor que 18°C esta frio')
+// //     prompt('esta frio')
+// // }else
+// //     alert("se temperatura estiver 18°C e 25°C esta agradavel")
+// //     prompt('temperatura esta agradavel')
+
+// // if(quente){
+// //     alert(' se for maior que 25°C esta quente')
+// //     prompt('esta quente ')
+// // }
+
+
+
+// //laço de repetição
+// // e  a execução de um bloco de codigo repetiçao,mediante a um condicao
+
+// // for quando eu sei a quantidade  de vezes que for rodar esse bloco e uso o for
+// //while rodo ate atender uma condição por exemplo quanto eu nao receber uma resposta especifica 
+// // e depois vejo se a codicao e atende 
+
+
+// //for 
+
+// // for(let i = 1; i<=10; i++){
+// //     alert(i)
+// // }
+// // // whlhe procura uma condição
+// // let contador =20
+// // while(contaddor<=40){
+// //     alert(contador)
+// //     contador ++
+// // }
+
+// // let i =1
+// // do{
+// //     if(i%2 !=0){
+
+    
+// //         alert(i)
+// //     }
+// //     i++
+// // }while(i <=20)
+
+
+
+
+//     // let contaddor =1
+//     // while(contaddor>=1){
+//     //     alert(contador)
+//     //     contaddr
+//     // }
+
+
+//     // let joogo=1
+//     // while (contador) {
+//     //     alert('jogo do papel tesoura ')
+//     //     prompt('quem vai ganhar')        
+//     // }
+
+
+
+// // alert('calcule o fatorial de um numero')
+// // prompt('5*5')
+// // prompt (`exibir ${resultado}`)
+// // {
+
+
+// // }
+
+// alert('ele e lindo')
+// prompt ('se ele e lindo fala sim')
+
+// alert('ele e feio')
+// prompt('se ele e feio diga sim')
